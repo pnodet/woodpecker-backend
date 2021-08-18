762ba86316d39c510df4150f016068ac116ca523
Paul Nodet <paul.nodet@gmail.com>
2021-08-18 19:43:42 +0200

    Add todo comments

--- routes/puzzles.js
+++ routes/puzzles.js
@@ -3,6 +3,28 @@ import * as queries from '../controllers/dbController.js';
 
 const router = Router();
 
-router.post('/', async (_req, _res) => {});
+// Create new set of puzzle
+router.get('/set', async (_req, _res) => {
+
+  // try to find 25 puzzles (not already in a set ?) of this user in the db
+
+  // create a object ? or an array ?
+  const set = {
+    puzzles: ['_id-01', '_id-02', '_id-03'],
+    tries: 3,
+    successRate,
+    bestTime,
+  };
+
+  // save it in DB
+
+});
+
+// Update a player's set (change best time, change number of try…) 
+router.put('/set', async (_req, _res) => {});
+
+// Get user last played set ?
+router.get('/set:UserId', async (_req, _res) => {});
+
 
 export default router;
--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,14 @@ import {Router} from 'express';
 
 const router = Router();
 
-router.post('/', async (_req, _res) => {});
+/** POST : Create a new user */ 
+router.post('/', async (req, res) => {
+  //check if user already exists
+  const testUser = await findOne({email: req.body.email});
+  if (testUser) res.status(400).send('Email already used')
+
+  // else
+  user = new User();
+});
 
 export default router;
