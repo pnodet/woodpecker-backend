b99af6306220aad8f0262e5a06093d0c8e50cab7
Paul Nodet <paul.nodet@gmail.com>
2021-08-19 12:05:48 +0200

    Change port, conflict with an other api

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const helmet = require('helmet');
 const session = require('express-session');
 
 /** APP */
+const port = 5669
 const app = express();
 app.use(json());
 app.use(urlencoded({extended: true}));
@@ -22,7 +23,7 @@ const auth = require('./routes/auth.js');
 app.use('/auth', auth);
 
 /** START SERVER */
-app.listen(3000, () => {
-  console.log('Server running on port 3000!');
+app.listen(port, () => {
+  console.log(`Server running on port ${port}!`);
   // TODO: connect to db, if it doesn't exist create collection…
 });
